55e4aa92b9d7eabb46b3a5d0434a98f3a2a2e215
Gerard Hernandez <ger.almenara@gmail.com>
2024-04-14 21:20:53 -0700

    Update .gitignore and package.json, and modify ts-metar-parser name and version

---
+++ dist/index.d.ts
@@ -0,0 +1,51 @@
+type MetarObject = {
+    raw_text: string;
+    raw_parts: string[];
+    icao?: string;
+    observed?: Date;
+    wind?: {
+        degrees: number;
+        degrees_from?: number;
+        degrees_to?: number;
+        speed_kts: number;
+        speed_mps: number;
+        gust_kts: number;
+        gust_mps: number;
+    };
+    visibility?: {
+        miles: string;
+        miles_float: number;
+        meters: string;
+        meters_float: number;
+    };
+    conditions?: {
+        code: string;
+    }[];
+    clouds?: {
+        code: string;
+        base_feet_agl: number;
+        base_meters_agl: number;
+    }[];
+    ceiling?: {
+        code: string;
+        feet_agl: number;
+        meters_agl: number;
+    };
+    temperature?: {
+        celsius: number;
+        fahrenheit: number;
+    };
+    dewpoint?: {
+        celsius: number;
+        fahrenheit: number;
+    };
+    humidity_percent?: number;
+    barometer?: {
+        hg: number;
+        kpa: number;
+        mb: number;
+    };
+    flight_category: string;
+};
+declare const metarParser: (metarString: string) => MetarObject;
+export default metarParser;
